eb5551508e6966ddcbe2574bd71bba11d5693641
gongbaodd@ProArt <gongbaodd@qq.com>
2025-09-17 17:42:15 +0300

    add Next piece

--- src/ui/Next.ts
+++ src/ui/Next.ts
@@ -1,9 +1,17 @@
-import { html, LitElement } from "lit";
+import { css, html, LitElement, unsafeCSS } from "lit";
 import { customElement } from "lit/decorators.js"
+import daisyCss from "../style.css?inline"
 
 @customElement("next-piece")
 export class Next extends LitElement {
+    static styles = css`${unsafeCSS(daisyCss)}`;
     render() {
-        return html`<div>Hello</div>`
+        return html`
+<div class="avatar">
+  <div class="w-24 rounded">
+    <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+  </div>
+</div>
+        `
     }
 }
